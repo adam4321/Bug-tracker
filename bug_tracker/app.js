f8c2f1db962845e89a4fb3abf1f201a5bd5d84d8
Adam Wright <adam@adamjwright.com>
2020-10-09 23:53:45 -0700

    add on passport setup

--- bug_tracker/app.js
+++ bug_tracker/app.js
@@ -11,6 +11,9 @@
 const express = require('express');
 const app = express();
 
+// PORT NUMBER - Set static port for the appliction 
+app.set('port', 5000);
+
 // Set up express-handlebars
 const handlebars = require('express-handlebars');
 app.set('view engine', '.hbs');
@@ -33,8 +36,18 @@ app.set('mysql', mysql);
 // Set up route to static files
 app.use('/bug_tracker', express.static('public'));
 
-// PORT NUMBER - Set static port for the appliction 
-app.set('port', 5000);
+// Set up cookie session and configure session storage
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+    name: 'session-name',
+    keys: ['key1', 'key2']
+}))
+
+// Include and configure passport
+const passport = require('passport');
+require('./passport.js');
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 /* PAGE ROUTES ---------------------------------------------------------------*/
@@ -61,19 +74,42 @@ app.use('/bug_tracker/projects', require('./routes/update-projects-page.js'));
 app.use('/bug_tracker/companies', require('./routes/update-company-page.js'));
 
 
+/* AUTHENTICATION ROUTES ---------------------------------------------------- */
+
+// REQUEST AUTH
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// POST-AUTH REDIRECT
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login/failed' }),
+    function(req, res) {
+        res.redirect('/dashboard');
+    }
+);
+
+// LOG OUT ROUTE - for all pages
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/login');
+})
+
+
 /* ERROR ROUTES -------------------------------------------------------------*/
 
+// Middleware - Function to Check user is Logged in
+const checkUserLoggedIn = (req, res, next) => {
+    req.user ? next(): res.status(401).render('unauthorized-page', {layout: 'login'});
+}
+
 // PAGE NOT FOUND - Route for bad path error page
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
+app.use(checkUserLoggedIn, (req, res) => {
+    res.status(404).render('404');
 });
    
 // INTERNAL SERVER ERROR - Route for a server-side error
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500);
-    res.render('500');
+    res.status(err.status || 500).render('500');
 });
 
 
@@ -81,5 +117,5 @@ app.use((err, req, res, next) => {
 
 // Set to render on a static port set globally
 app.listen(app.get('port'), () => {
-    console.log(`\nExpress started at http://localhost:${app.get('port')}/bug_tracker/\nPress ctrl-C to terminate.\n`);
+    console.log(`\nExpress started at http://localhost:${app.get('port')}/bug_tracker/login\nPress ctrl-C to terminate.\n`);
 });
