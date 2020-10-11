a5f5a07e77ec7c8ec5f1285d90bd1f317a2b82da
Adam Wright <adam@adamjwright.com>
2020-10-10 23:56:30 -0700

    work on rendering table

--- routes/user-home.js
+++ routes/user-home.js
@@ -18,39 +18,76 @@ const router = express.Router();
 
 /* USER BUGS - Function to render user's bugs ------------------------------ */
 function renderHome(req, res) {
-    const query = `SELECT firstName, lastName, programmerId FROM Programmers WHERE programmerId = ?`
+    const sql_query_1 = `SELECT firstName, lastName, programmerId FROM Programmers WHERE programmerId = ?`
+
+    // 2nd query populates the bug list
+    const sql_query_2 = `SELECT p.firstName, p.lastName, b.bugId, pj.projectName, b.bugSummary, b.bugDescription, 
+                        b.dateStarted, b.resolution, b.priority, b.fixed 
+	                    FROM Programmers p 
+		                JOIN Bugs_Programmers bp ON p.programmerId = bp.programmerId
+		                JOIN Bugs b ON bp.bugId = b.bugId
+                        LEFT OUTER JOIN Projects pj ON b.projectId <=> pj.projectId
+                            ORDER BY bugId`;
+
     const mysql = req.app.get('mysql');
 
+    // Initialize empty context array
+    let context = req.user;
+
     // See if user with email at end of query string exists in database
-    mysql.pool.query(query, req.user.id,
-    function(err, rows, fields) {
+    mysql.pool.query(sql_query_1, req.user.id, (err, rows, fields) => {
         if (err) {
            next(err);
            return;
         }
         
-        // Initialize empty context array
-        let context = req.user;
-        
         // If the user does not exist in the database, render user-account page
         if (rows.length === 0) {
-		    context.uid = req.query.uid;    
             res.render("user-account", context);
         }
 	    
         // Otherwise, render user home page
-        else
-	    {
-		    context.name = rows[0].firstName + " " + rows[0].lastName;
-            context.uid = req.query.uid;
-            res.render("user-home", context);
+        else {
+            mysql.pool.query(sql_query_2, (err, rows) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
+        
+                let prevEntryBugId;           // Cache the previous entry's id to avoid duplication
+                let bugProgrammers = [];      // Hold the programmers for each entry
+                let bugsDbData = [];          // Put the mysql data into an array for rendering
+        
+                for (let i in rows) {
+                    // If this is the same entry as the last, then only add the programmer to the array
+                    if (prevEntryBugId == rows[i].bugId) {
+                        bugProgrammers.push(rows[i].firstName + ' ' + rows[i].lastName);
+                    }
+                    // This is a new entry
+                    else {
+                        prevEntryBugId = rows[i].bugId;         // Cache the bugId
+                        bugProgrammers = [];                    // Add the programmer to the array
+                        bugProgrammers.push(rows[i].firstName + ' ' + rows[i].lastName);
+        
+                        // Push a single entry
+                        bugsDbData.push({
+                            bugId: rows[i].bugId,
+                            bugSummary: rows[i].bugSummary,
+                            bugDescription: rows[i].bugDescription,
+                            projectName: rows[i].projectName,
+                            programmers: bugProgrammers,
+                            dateStarted: rows[i].dateStarted,
+                            priority: rows[i].priority,
+                            fixed: rows[i].fixed,
+                            resolution: rows[i].resolution
+                        });
+                    }
+                }
+
+                res.render("user-home", context);
+            });
 	    }
     });
-
-    // let context = {};
-    // context = req.user;
-    // console.log(context);
-    // res.render("user-home", context);
 };
 
 
