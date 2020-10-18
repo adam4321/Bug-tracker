db3ea8e6e67d9a9024d1b366187ccf9813abd75c
Adam Wright <adam@adamjwright.com>
2020-10-18 04:17:04 -0700

    update dependencies

--- passport.js
+++ passport.js
@@ -3,12 +3,12 @@
 **                passport.js
 ******************************************************************************/
 
-const AUTH_CREDS = require('./AUTH-credentials.js');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-const FACE_CREDS = require('./credentials.js')
-const FacebookStrategy = require('passport-facebook').Strategy;
+// Include Oauth2 strategies
+const CREDS = require('./AUTH-credentials.js');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+// const FacebookStrategy = require('passport-facebook').Strategy;
 
 // Process the user's token
 passport.serializeUser(function(user, done) {
@@ -21,22 +21,22 @@ passport.deserializeUser(function(user, done) {
 
 // Create Oauth2 strategies
 passport.use(new GoogleStrategy({
-        clientID: GOOG_CREDS.GOOGLE_CLIENT_ID,
-        clientSecret: GOOG_CREDS.GOOGLE_CLIENT_SECRET,
-        callbackURL: GOOG_CREDS.GOOGLE_CALLBACK_URL
+        clientID: CREDS.GOOGLE_CLIENT_ID,
+        clientSecret: CREDS.GOOGLE_CLIENT_SECRET,
+        callbackURL: CREDS.GOOGLE_CALLBACK_URL
     },
     function(accessToken, refreshToken, profile, done) {
         return done(null, profile);
     }
 ));
 
-passport.use(new FacebookStrategy({
-        clientID: FACE_CREDS.FACEBOOK_CLIENT_ID,
-        clientSecret: FACE_CREDS.FACEBOOK_CLIENT_SECRET,
-        callbackURL: FACE_CREDS.FACEBOOK_CALLBACK_URL,
-        profileFields: ['id', 'displayName', 'photos', 'email']
-    }, 
-    function(accessToken, refreshToken, profile, done) {
-        return done(null, profile);
-    }
-));
+// passport.use(new FacebookStrategy({
+//         clientID: CREDS.FACEBOOK_CLIENT_ID,
+//         clientSecret: CREDS.FACEBOOK_CLIENT_SECRET,
+//         callbackURL: CREDS.FACEBOOK_CALLBACK_URL,
+//         profileFields: ['id', 'displayName', 'photos', 'email']
+//     }, 
+//     function(accessToken, refreshToken, profile, done) {
+//         return done(null, profile);
+//     }
+// ));
