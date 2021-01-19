661c7a39f854ba6854f4a8bfe9dbdae6c06ad9c2
Adam Wright <adam@adamjwright.com>
2021-01-19 01:24:21 -0800

    fix no records js errors

--- public/js/your-bugs.js
+++ public/js/your-bugs.js
@@ -2,234 +2,237 @@
 **  Description: USER-HOME client-side JavaScript file
 **************************************************************/
 
+try {
+    /* DELETE BUG CLIENT SIDE -------------------------------------------------- */
 
-/* DELETE BUG CLIENT SIDE -------------------------------------------------- */
+    // Function call to delete a row from bug
+    function deleteBug(tbl, curRow, bugId) {
+        let table = document.getElementById(tbl);
+        let rowCount = table.rows.length;
+        let req = new XMLHttpRequest();
+        let path = "/bug_tracker/all_bugs/deleteBug";
 
-// Function call to delete a row from bug
-function deleteBug(tbl, curRow, bugId) {
-    let table = document.getElementById(tbl);
-    let rowCount = table.rows.length;
-    let req = new XMLHttpRequest();
-    let path = "/bug_tracker/all_bugs/deleteBug";
+        reqBody = JSON.stringify({bugId: bugId});
 
-    reqBody = JSON.stringify({bugId: bugId});
-
-    req.open("POST", path, true);
-    req.setRequestHeader("Content-Type", "application/json");
-    req.addEventListener("load", () => {
-        if (req.status >= 200 && req.status < 400) {
-            for (let i = 0; i < rowCount; i++) {
-                let row = table.rows[i];
-        
-                if (row == curRow.parentNode.parentNode) {
-                    table.deleteRow(i);
+        req.open("POST", path, true);
+        req.setRequestHeader("Content-Type", "application/json");
+        req.addEventListener("load", () => {
+            if (req.status >= 200 && req.status < 400) {
+                for (let i = 0; i < rowCount; i++) {
+                    let row = table.rows[i];
+            
+                    if (row == curRow.parentNode.parentNode) {
+                        table.deleteRow(i);
+                    }
                 }
+                updateChartDelete();
+            } 
+            else {
+                console.error("Delete request error");
             }
-            updateChartDelete();
-        } 
-        else {
-            console.error("Delete request error");
+        });
+
+        req.send(reqBody);
+    }
+
+    /* VIEW ALL BUGS CLIENT SIDE ----------------------------------------------- */
+
+    // Function call to clear search results
+    let viewAllButton = document.getElementById("clear-search");
+    viewAllButton.setAttribute('onclick', 'viewAllBugs()');
+
+    function viewAllBugs() {
+        path = "/bug_tracker/all_bugs/viewAllBugs";
+        let req = new XMLHttpRequest();
+
+        req.open("POST", path, true);   
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send();
+
+        req.addEventListener("load", () => {
+            if (req.status >= 200 && req.status < 400) {
+                let bugsArray = JSON.parse(req.responseText).bugs;
+
+                // clear table before building search results
+                let tableBody = document.getElementById("table-body");
+                tableBody.innerHTML = '';
+
+                // if no existing bugs
+                if(bugsArray.length == 0) {
+                    let newRow = tableBody.insertRow(-1);
+                    let summaryCell = document.createElement('td');
+                    summaryCell.textContent = "No current bugs!";
+                    summaryCell.style.color = "#ff0000";
+                    summaryCell.className = 'mdl-data-table__cell--non-numeric'; 
+                    newRow.appendChild(summaryCell);
+                    return;
+                }
+
+                // build rows for each bug if there is at least one result
+                bugsArray.forEach(element => {
+                    createRow(tableBody, element);
+                });
+
+            } else {
+                console.error("View all bugs: request error.");
+            }
+        });
+    }
+
+
+    /* SEARCH BUG CLIENT SIDE -------------------------------------------------- */
+
+    // Function call to search bug table for substring
+    let searchButton = document.getElementById("search-btn");
+    searchButton.addEventListener('click', searchBug);
+
+    function searchBug() {
+        let searchString = document.getElementById("search-input").value;
+        let path = "/bug_tracker/all_bugs/searchBug";
+        let req = new XMLHttpRequest();
+        let reqBody = JSON.stringify({searchString: searchString});
+
+        req.open("POST", path, true);   
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(reqBody); 
+
+        req.addEventListener("load", () => {
+            if (req.status >= 200 && req.status < 400) {
+                let bugsArray = JSON.parse(req.responseText).bugs;
+
+                // clear table before building search results
+                let tableBody = document.getElementById("table-body");
+                tableBody.innerHTML = '';
+
+                // if no results are found
+                if(bugsArray.length == 0) {
+                    let newRow = tableBody.insertRow(-1);
+                    let summaryCell = document.createElement('td');
+                    summaryCell.textContent = "No results found!";
+                    summaryCell.style.color = "#ff0000";
+                    summaryCell.className = 'mdl-data-table__cell--non-numeric'; 
+                    newRow.appendChild(summaryCell);
+                    return;
+                }
+
+                // build rows for each bug if there is at least one result
+                bugsArray.forEach(element => {
+                    createRow(tableBody, element);
+                });
+
+                // Clear the searchbar
+                document.getElementById('search-form').reset()
+
+            } else {
+                console.error("Search request error.");
+            }
+        });
+    };
+
+
+    // Function to create new bug row after searching
+    function createRow(tableBody, bugData) {
+        let newRow = tableBody.insertRow(-1);
+
+        // Bug Summary element
+        let summaryCell = document.createElement('td');
+        summaryCell.className = 'mdl-data-table__cell--non-numeric';
+        let innerCell = document.createElement('div');
+        summaryCell.appendChild(innerCell);
+        innerCell.textContent = bugData.bugSummary;
+        innerCell.className = 'user-text';
+        newRow.appendChild(summaryCell);
+
+        // Bug Description element
+        let descriptionCell = document.createElement('td');
+        descriptionCell.className = 'mdl-data-table__cell--non-numeric';
+        innerCell = document.createElement('div');
+        descriptionCell.appendChild(innerCell);
+        innerCell.textContent = bugData.bugDescription;
+        innerCell.className = 'user-text';
+        newRow.appendChild(descriptionCell);
+
+        // Project element
+        let projectCell = document.createElement('td');
+        projectCell.className = 'mdl-data-table__cell--non-numeric'; 
+        projectCell.textContent = bugData.projectName;
+        if(projectCell.textContent == "") {
+            projectCell.textContent = "NULL";
+        }
+        newRow.appendChild(projectCell);
+
+        // Programmers element
+        let programmerCell = document.createElement('td');
+        programmerCell.className = 'mdl-data-table__cell--non-numeric';
+        let cellString = '';
+        for (i = 0; i < bugData.programmers.length; i++) {
+            cellString += bugData.programmers[i] + '<br>';
+        }
+        programmerCell.innerHTML = cellString;
+        newRow.appendChild(programmerCell);
+
+        // Date started element
+        let dateCell = document.createElement('td');
+        dateCell.textContent = bugData.dateStarted;
+        newRow.appendChild(dateCell);
+
+        // Priority element
+        let priorityCell = document.createElement('td');
+        priorityCell.textContent = bugData.priority;
+        newRow.appendChild(priorityCell);
+
+        // Fixed element
+        let fixedCell = document.createElement('td');
+        fixedCell.className = 'bugFixed';
+        if (bugData.fixed == 0) {
+            fixedCell.textContent = " No "
+        } else {
+            fixedCell.textContent = " Yes ";
+        }
+        newRow.appendChild(fixedCell);
+
+        // Resolution element
+        let resolutionCell = document.createElement('td');
+        resolutionCell.className = 'mdl-data-table__cell--non-numeric';
+        innerCell = document.createElement('div');
+        resolutionCell.appendChild(innerCell);
+        innerCell.textContent = bugData.resolution;
+        innerCell.className = 'user-text';
+        newRow.appendChild(resolutionCell);
+
+        // Update button element
+        let updateCell = document.createElement('td');
+        newRow.appendChild(updateCell);
+        let updateBtn = document.createElement('a');
+        updateCell.appendChild(updateBtn);
+        updateBtn.text = "Update";
+        updateBtn.className = "update-btn";
+        updateBtn.href = `/bug_tracker/edit_bug?bugId=${bugData.bugId}`;
+
+        // Delete button element
+        let deleteCell = document.createElement('td');
+        newRow.appendChild(deleteCell);
+        let deleteBtn = document.createElement('a');
+        deleteCell.appendChild(deleteBtn);
+        deleteBtn.type = "button";
+        deleteBtn.text = "Delete";
+        deleteBtn.className = "update-btn";
+        deleteBtn.setAttribute('onclick', `deleteBug('recordTable', this, ${bugData.bugId})`);
+    }
+
+    // Change behavior of pressing 'Enter' on search bar. 
+    let searchInput = document.getElementById("search-input");
+
+    searchInput.addEventListener('keydown', function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById('search-btn').click();
         }
     });
-
-    req.send(reqBody);
 } 
-
-
-/* VIEW ALL BUGS CLIENT SIDE ----------------------------------------------- */
-
-// Function call to clear search results
-let viewAllButton = document.getElementById("clear-search");
-viewAllButton.setAttribute('onclick', 'viewAllBugs()');
-
-function viewAllBugs() {
-    path = "/bug_tracker/all_bugs/viewAllBugs";
-    let req = new XMLHttpRequest();
-
-    req.open("POST", path, true);   
-    req.setRequestHeader("Content-Type", "application/json");
-    req.send();
-
-    req.addEventListener("load", () => {
-        if (req.status >= 200 && req.status < 400) {
-            let bugsArray = JSON.parse(req.responseText).bugs;
-
-            // clear table before building search results
-            let tableBody = document.getElementById("table-body");
-            tableBody.innerHTML = '';
-
-            // if no existing bugs
-            if(bugsArray.length == 0) {
-                let newRow = tableBody.insertRow(-1);
-                let summaryCell = document.createElement('td');
-                summaryCell.textContent = "No current bugs!";
-                summaryCell.style.color = "#ff0000";
-                summaryCell.className = 'mdl-data-table__cell--non-numeric'; 
-                newRow.appendChild(summaryCell);
-                return;
-            }
-
-            // build rows for each bug if there is at least one result
-            bugsArray.forEach(element => {
-                createRow(tableBody, element);
-            });
-
-        } else {
-            console.error("View all bugs: request error.");
-        }
-    });
+catch {
+    console.log("You can assign yourself to bugs on the All Bugs page");
 }
-
-
-/* SEARCH BUG CLIENT SIDE -------------------------------------------------- */
-
-// Function call to search bug table for substring
-let searchButton = document.getElementById("search-btn");
-searchButton.addEventListener('click', searchBug);
-
-function searchBug() {
-    let searchString = document.getElementById("search-input").value;
-    let path = "/bug_tracker/all_bugs/searchBug";
-    let req = new XMLHttpRequest();
-    let reqBody = JSON.stringify({searchString: searchString});
-
-    req.open("POST", path, true);   
-    req.setRequestHeader("Content-Type", "application/json");
-    req.send(reqBody); 
-
-    req.addEventListener("load", () => {
-        if (req.status >= 200 && req.status < 400) {
-            let bugsArray = JSON.parse(req.responseText).bugs;
-
-            // clear table before building search results
-            let tableBody = document.getElementById("table-body");
-            tableBody.innerHTML = '';
-
-            // if no results are found
-            if(bugsArray.length == 0) {
-                let newRow = tableBody.insertRow(-1);
-                let summaryCell = document.createElement('td');
-                summaryCell.textContent = "No results found!";
-                summaryCell.style.color = "#ff0000";
-                summaryCell.className = 'mdl-data-table__cell--non-numeric'; 
-                newRow.appendChild(summaryCell);
-                return;
-            }
-
-            // build rows for each bug if there is at least one result
-            bugsArray.forEach(element => {
-                createRow(tableBody, element);
-            });
-
-            // Clear the searchbar
-            document.getElementById('search-form').reset()
-
-        } else {
-            console.error("Search request error.");
-        }
-    });
-};
-
-
-// Function to create new bug row after searching
-function createRow(tableBody, bugData) {
-    let newRow = tableBody.insertRow(-1);
-
-    // Bug Summary element
-    let summaryCell = document.createElement('td');
-    summaryCell.className = 'mdl-data-table__cell--non-numeric';
-    let innerCell = document.createElement('div');
-    summaryCell.appendChild(innerCell);
-    innerCell.textContent = bugData.bugSummary;
-    innerCell.className = 'user-text';
-    newRow.appendChild(summaryCell);
-
-    // Bug Description element
-    let descriptionCell = document.createElement('td');
-    descriptionCell.className = 'mdl-data-table__cell--non-numeric';
-    innerCell = document.createElement('div');
-    descriptionCell.appendChild(innerCell);
-    innerCell.textContent = bugData.bugDescription;
-    innerCell.className = 'user-text';
-    newRow.appendChild(descriptionCell);
-
-    // Project element
-    let projectCell = document.createElement('td');
-    projectCell.className = 'mdl-data-table__cell--non-numeric'; 
-    projectCell.textContent = bugData.projectName;
-    if(projectCell.textContent == "") {
-        projectCell.textContent = "NULL";
-    }
-    newRow.appendChild(projectCell);
-
-    // Programmers element
-    let programmerCell = document.createElement('td');
-    programmerCell.className = 'mdl-data-table__cell--non-numeric';
-    let cellString = '';
-    for (i = 0; i < bugData.programmers.length; i++) {
-        cellString += bugData.programmers[i] + '<br>';
-    }
-    programmerCell.innerHTML = cellString;
-    newRow.appendChild(programmerCell);
-
-    // Date started element
-    let dateCell = document.createElement('td');
-    dateCell.textContent = bugData.dateStarted;
-    newRow.appendChild(dateCell);
-
-    // Priority element
-    let priorityCell = document.createElement('td');
-    priorityCell.textContent = bugData.priority;
-    newRow.appendChild(priorityCell);
-
-    // Fixed element
-    let fixedCell = document.createElement('td');
-    fixedCell.className = 'bugFixed';
-    if (bugData.fixed == 0) {
-        fixedCell.textContent = " No "
-    } else {
-        fixedCell.textContent = " Yes ";
-    }
-    newRow.appendChild(fixedCell);
-
-    // Resolution element
-    let resolutionCell = document.createElement('td');
-    resolutionCell.className = 'mdl-data-table__cell--non-numeric';
-    innerCell = document.createElement('div');
-    resolutionCell.appendChild(innerCell);
-    innerCell.textContent = bugData.resolution;
-    innerCell.className = 'user-text';
-    newRow.appendChild(resolutionCell);
-
-    // Update button element
-    let updateCell = document.createElement('td');
-    newRow.appendChild(updateCell);
-    let updateBtn = document.createElement('a');
-    updateCell.appendChild(updateBtn);
-    updateBtn.text = "Update";
-    updateBtn.className = "update-btn";
-    updateBtn.href = `/bug_tracker/edit_bug?bugId=${bugData.bugId}`;
-
-    // Delete button element
-    let deleteCell = document.createElement('td');
-    newRow.appendChild(deleteCell);
-    let deleteBtn = document.createElement('a');
-    deleteCell.appendChild(deleteBtn);
-    deleteBtn.type = "button";
-    deleteBtn.text = "Delete";
-    deleteBtn.className = "update-btn";
-    deleteBtn.setAttribute('onclick', `deleteBug('recordTable', this, ${bugData.bugId})`);
-}
-
-// Change behavior of pressing 'Enter' on search bar. 
-let searchInput = document.getElementById("search-input");
-
-searchInput.addEventListener('keydown', function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById('search-btn').click();
-    }
-});
 
 
 /* FIXED BUGS DOUGHNUT CHART ----------------------------------------------- */
@@ -301,11 +304,19 @@ function printChart(fixedCount, brokenCount, countSize) {
 
 // Function to walk the bugs table and gather a count of fixed and broken bugs
 function countFixedVals() {
-    let recordTable = document.getElementById('recordTable');
-    let cells = document.getElementsByClassName('bugFixed')
-
-    let count = recordTable.rows.length - 1;
+    let count = 0;
     let fixedCount = 0;
+    let cells;
+
+    // Catch the values when a user has no assigned bugs
+    try {
+        let recordTable = document.getElementById('recordTable');
+        cells = document.getElementsByClassName('bugFixed');
+        count = recordTable.rows.length - 1;
+    }
+    catch {
+        console.log('The chart defaults to 100% when there are no assigned bugs');
+    }
     
     // Gather the number of fixed bugs
     for (let i = 0; i < cells.length; i++) {
@@ -320,8 +331,8 @@ function countFixedVals() {
 
     // Calculate the number of broken bugs
     let brokenCount = count - fixedCount;
-
     let fixedObj = {fixed: fixedCount, broken: brokenCount, count: count};
+    
     return fixedObj;
 }
 
