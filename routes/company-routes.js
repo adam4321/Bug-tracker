bf8dd0c224ee8f0a9421d02bae31809414b39317
Adam Wright <adam@adamjwright.com>
2021-03-02 04:29:53 -0800

    Separate add form

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -54,6 +54,41 @@ function displayCompanyPage(req, res, next) {
 }
 
 
+/* ADD COMPANIES PAGE RENDER - function to view all existing companies ----- */
+function displayAddCompanyPage(req, res, next) {
+    // Find all of the current companies
+    const mysql = req.app.get('mysql');
+    let sql_query = `SELECT * FROM Companies`;
+
+    // Initialize empty context object with Google user props
+    let context = {};
+    context.id = req.user.id;
+    context.email = req.user.email;
+    context.name = req.user.displayName;
+    context.photo = req.user.picture;
+    context.accessLevel = req.session.accessLevel;
+
+    mysql.pool.query(sql_query, (err, rows) => {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        // Put the mysql data into an array for rendering
+        let companyDbData = [];
+        for (let i in rows) {
+            companyDbData.push({
+                companyId: rows[i].companyId,
+                companyName: rows[i].companyName,
+                dateJoined: rows[i].dateJoined,
+            });
+        }
+        context.companies = companyDbData;
+        res.render('add-company', context);
+    });
+}
+
+
 /* COMPANIES PAGE INSERT NEW COMPANY - function to insert a new company ---- */
 function submitCompany(req, res, next) {
     const mysql = req.app.get('mysql');
@@ -75,6 +110,7 @@ function submitCompany(req, res, next) {
 /* COMPANIES PAGE ROUTES ----------------------------------------------------- */
 
 router.get('/', checkUserLoggedIn, displayCompanyPage);
-router.post('/insertCompany', checkUserLoggedIn, submitCompany);
+router.get('/add_company', checkUserLoggedIn, displayAddCompanyPage);
+router.post('/add_company/insertCompany', checkUserLoggedIn, submitCompany);
 
 module.exports = router;
