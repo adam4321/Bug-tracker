7a3d438321d272f9fd0927d10ee79cbc63b322b6
Adam Wright <adam@adamjwright.com>
2021-03-02 04:31:47 -0800

    Separate add form

--- routes/projects-routes.js
+++ routes/projects-routes.js
@@ -72,6 +72,65 @@ function renderProjects(req, res, next) {
             // After the 2 calls return, then populate the context array
             context.companies = companyDbData;
             context.projects = projectDbData;
+            res.render('projects', context);
+        });
+    });
+}
+
+
+/* RENDER ADD PROJECTS PAGE - Function to render the project page ---------- */
+function renderAddProjects(req, res, next) {
+    // Find all of the projects and their associated companies
+    let sql_query_2 = `SELECT * FROM Projects AS p JOIN Companies AS c ON p.companyId = c.companyId`;
+    let sql_query_1 = `SELECT companyId, companyName FROM Companies`;
+
+    const mysql = req.app.get('mysql');
+    
+    // Initialize empty context object with Google user props
+    let context = {};
+    context.id = req.user.id;
+    context.email = req.user.email;
+    context.name = req.user.displayName;
+    context.photo = req.user.picture;
+    context.accessLevel = req.session.accessLevel;
+
+    // Query projects
+    mysql.pool.query(sql_query_2, (err, rows) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        
+        // Put the mysql data into an array for rendering 
+        let projectDbData = [];
+        for (let i in rows) {
+            projectDbData.push({
+                projectId: rows[i].projectId,
+                projectName: rows[i].projectName,
+                companyName: rows[i].companyName,
+                dateStarted: rows[i].dateStarted,
+                lastUpdated: rows[i].lastUpdated,
+                inMaintenance: rows[i].inMaintenance
+            });
+        }
+
+        // Query for the list of companies
+        mysql.pool.query(sql_query_1,  (err, rows) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            let companyDbData = [];
+            for (let i in rows) {
+                companyDbData.push({
+                    companyId: rows[i].companyId,
+                    companyName: rows[i].companyName
+                });
+            }
+            
+            // After the 2 calls return, then populate the context array
+            context.companies = companyDbData;
+            context.projects = projectDbData;
             res.render('add-project', context);
         });
     });
@@ -108,6 +167,7 @@ function submitProject(req, res, next) {
 /* PROJECTS PAGE ROUTES ---------------------------------------------------- */
 
 router.get('/', checkUserLoggedIn, renderProjects);
+router.get('/add_project', checkUserLoggedIn, renderAddProjects);
 router.post('/insertProject', checkUserLoggedIn, submitProject);
 
 module.exports = router;
