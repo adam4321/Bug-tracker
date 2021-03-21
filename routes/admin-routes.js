01ce55abfc8f88970a3c9253b10f7b3590f10815
Adam Wright <adam@adamjwright.com>
2021-03-20 18:59:07 -0700

    Update reset route

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -4,7 +4,6 @@
 **  Root path:   localhost:5000/bug_tracker/settings
 **
 **  Contains:    /
-**               /update-user
 **               /resetTable
 **              
 **  SECURED ROUTES!  --  All routes must call checkUserLoggedIn 
@@ -31,75 +30,6 @@ function renderSettings(req, res, next) {
     context.accessLevel = req.session.accessLevel;
 
     res.render("admin", context);
-};
-
-
-/* UPDATE USER SETTINGS - Function to render update account page ----------- */
-function renderUpdateForm(req, res) {
-    const query = `SELECT programmerId, firstName, lastName, email, mobile_number, dateStarted, accessLevel
-                    FROM Programmers
-                    WHERE programmerId = ?`
-    const mysql = req.app.get('mysql');
-
-    // See if user with email at end of query string exists in database
-    mysql.pool.query(query, decodeURIComponent([req.query.uid]),
-        function(err, rows, fields) {
-            if (err) {
-                next(err);
-                return;
-            }
-            
-            // If the user does not exist in the database, redirect to login page
-            if (rows.length === 0) {
-                res.render('login-redirect');
-            }
-            
-            // Otherwise, pull the user's information from returned results to send to handlebars page
-            else {
-                // Initialize empty context array
-                let context = [];
-            
-                // Fill context array
-                context.uid = req.query.uid;
-                context.firstName = rows[0].firstName;
-                context.lastName = rows[0].lastName;
-                context.email = rows[0].email;
-                context.phone = rows[0].mobile_number;
-                context.dateStarted = rows[0].dateStarted;
-                context.accessLevel = rows[0].accessLevel;
-
-                // Render update account page
-                res.render("update-account", context); 
-            }
-        }
-    );
-};
-
-
-/* UPDATE USER SETTINGS - Function to update settings for an existing user - */
-function updateData(req, res, next) {
-    const query = `UPDATE Programmers SET firstName = ?, lastName = ?, email = ?, mobile_number = ?, dateStarted = ?
-                    WHERE programmerId = ?`
-    const mysql = req.app.get('mysql');
-    
-    mysql.pool.query(query, 
-        [
-            req.body.firstName,
-            req.body.lastName,
-            req.body.email,
-            req.body.mobile_number, 
-            req.body.dateStarted, 
-            req.body.uid 
-        ], 
-        function(err, result) {
-            if (err) {
-                next(err);
-                return;
-            }
-            
-            res.end()
-        }
-    );
 };
 
 
@@ -170,8 +100,6 @@ function resetTable(req, res, next) {
 /* UPDATE ACCOUNT PAGE ROUTES ---------------------------------------------- */
 
 router.get('/', checkUserLoggedIn, renderSettings);
-router.get('/update-account', checkUserLoggedIn, renderUpdateForm);
-router.post('/update-user', checkUserLoggedIn, updateData);
 router.post('/resetTable', checkUserLoggedIn, resetTable);
 
 module.exports = router;
