9abf7446039606efb63e333a1ed9f55a9e4d98ae
Adam Wright <adam@adamjwright.com>
2021-03-22 14:24:57 -0700

    Add input validation for checkboxes

--- public/js/add-bug.js
+++ public/js/add-bug.js
@@ -2,6 +2,43 @@
 **  Description: ADD-BUG client-side JavaScript file
 **************************************************************/
 
+
+/* INSERT BUG VERIFY PROGRAMMER CHECKED -------------------------------------*/
+
+// Function to verify that at least 1 programmer is checked
+function checkBoxChecked() {
+    const form = document.querySelector('#recordForm');
+    const checkboxes = form.querySelectorAll('input[type=checkbox]');
+    const checkboxLength = checkboxes.length;
+    const firstCheckbox = checkboxLength > 0 ? checkboxes[0] : null;
+
+    function init() {
+        if (firstCheckbox) {
+            for (let i = 0; i < checkboxLength; i++) {
+                checkboxes[i].addEventListener('change', checkValidity);
+            }
+
+            checkValidity();
+        }
+    }
+
+    function isChecked() {
+        for (let i = 0; i < checkboxLength; i++) {
+            if (checkboxes[i].checked) return true;
+        }
+
+        return false;
+    }
+
+    function checkValidity() {
+        const errorMessage = !isChecked() ? 'At least one checkbox must be selected.' : '';
+        firstCheckbox.setCustomValidity(errorMessage);
+    }
+
+    init();
+};
+
+
 /* INSERT BUG CLIENT SIDE -------------------------------------------------- */
 
 // Function to submit the bug's form data
