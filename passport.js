c258660a990a30a0a0df95fe959fc84523d18d16
Adam Wright <adam@adamjwright.com>
2021-04-05 18:00:14 -0700

    Remove unused Facebook login strategy

--- passport.js
+++ passport.js
@@ -8,7 +8,6 @@ const passport = require('passport');
 // Include Oauth2 strategies
 const CREDS = require('./AUTH-credentials.js');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
 
 // Process the user's token
 passport.serializeUser(function(user, done) {
@@ -25,17 +24,6 @@ passport.use(new GoogleStrategy({
         clientSecret: CREDS.GOOGLE_CLIENT_SECRET,
         callbackURL: CREDS.GOOGLE_CALLBACK_URL
     },
-    function(accessToken, refreshToken, profile, done) {
-        return done(null, profile);
-    }
-));
-
-passport.use(new FacebookStrategy({
-        clientID: CREDS.FACEBOOK_CLIENT_ID,
-        clientSecret: CREDS.FACEBOOK_CLIENT_SECRET,
-        callbackURL: CREDS.FACEBOOK_CALLBACK_URL,
-        profileFields: ['id', 'displayName', 'photos', 'email']
-    }, 
     function(accessToken, refreshToken, profile, done) {
         return done(null, profile);
     }
